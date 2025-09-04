8cdee73a9567c7be0b9dc2d1148f2a82ace610c7
read-ex <readexindia@gmail.com>
2025-09-04 12:52:10 +0530

    Update redircts.js

--- js/redircts.js
+++ js/redircts.js
@@ -1,11 +1,12 @@
 document.querySelectorAll('.gotoproductpage').forEach(function(btn) {
     btn.addEventListener('click', function() {
-        window.location.href = "/product.html";
+        window.location.href = "product.html";
     });
 });
  document.querySelector('.gotoshop').addEventListener('click', function() {
-        window.location.href = "/shop.html";
+        window.location.href = "shop.html";
     });
 function checkOut(){
-    window.location.href = "/address.html";
+    window.location.href = "address.html";
+
 }
